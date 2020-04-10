92f261dbdc6d941daaa87cc486cc6ebf694f0c70
Jason Motylinski <jason.motylinski@blackrock.com>
2020-04-10 06:33:33 -0400

    fix: layout of edit screen

--- ckanext/dictionary/fanstatic/dictionary.js
+++ ckanext/dictionary/fanstatic/dictionary.js
@@ -1,25 +1,32 @@
+reindex=function(){
+    $('.control-group').each(function(idx, el){
+        $(el).find('input').each(function(idx2, el2){
+            $(el2).attr('id',$(el2).attr('class') + "-" + idx);
+            $(el2).attr('name', $(el2).attr('class') + "_" + idx);
+        });
+    });
+};
+
 ckan.module('dictionary_add_field', function ($) {
     return {
       initialize: function () {
         $(function(){
-
-            reindex=function(){
-                $('.control-group').each(function(idx, el){
-                    $(el).find('input').each(function(idx2, el2){
-                        $(el2).attr('id',$(el2).attr('class') + "-" + idx);
-                        $(el2).attr('name', $(el2).attr('class') + "_" + idx);
-                    });
-                });
-            };
-
-            $('.btn-remove').click(function(el){
-                $(el).closest('.control-group').remove();
-                reindex();
-            });
-
             $('#btn-add').click(function() {
                 $('.control-group:last').after($('.control-group:last').clone());
                 $('.control-group:last input').val('');
+                reindex();
+            });
+        });
+      }
+    };
+});
+
+ckan.module('dictionary_remove_field', function ($) {
+    return {
+      initialize: function () {
+        $(function(){
+            $('.btn-remove').click(function(el){
+                $(el).closest('.control-group').remove();
                 reindex();
             });
         });
