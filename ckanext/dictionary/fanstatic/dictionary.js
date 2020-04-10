351360c9e0cd629061109f12c34918a2a3f0bd4f
Jason Motylinski <jason.motylinski@blackrock.com>
2020-04-10 06:15:46 -0400

    fix: reindex form fields so data saves correctly

--- ckanext/dictionary/fanstatic/dictionary.js
+++ ckanext/dictionary/fanstatic/dictionary.js
@@ -3,10 +3,17 @@ ckan.module('dictionary_add_field', function ($) {
       initialize: function () {
         $(function(){
             $( "#btn-add" ).click(function() {
-              $('.control-group:last').after($('.control-group:last').clone());
-              $('.control-group:last input').val('');
+                $('.control-group:last').after($('.control-group:last').clone());
+                $('.control-group:last input').val('');
+
+                $('.control-group').each(function(idx, el){
+                    $(el).find('input').each(function(idx2, el2){
+                        $(el2).attr('id',$(el2).attr('class') + "-" + idx);
+                        $(el2).attr('name', $(el2).attr('class') + "_" + idx);
+                    });
+                });
             });
-          });
+        });
       }
     };
 });
