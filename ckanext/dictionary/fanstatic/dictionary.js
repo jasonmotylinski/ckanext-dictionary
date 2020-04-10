2094d29e0f84472c09032d0a9d44e41e4cb81a76
Jason Motylinski <jason.motylinski@blackrock.com>
2020-04-10 06:29:10 -0400

    fix: Change layout for remoe and add buttons. Wire up JS for remoev

--- ckanext/dictionary/fanstatic/dictionary.js
+++ ckanext/dictionary/fanstatic/dictionary.js
@@ -2,16 +2,25 @@ ckan.module('dictionary_add_field', function ($) {
     return {
       initialize: function () {
         $(function(){
-            $( "#btn-add" ).click(function() {
-                $('.control-group:last').after($('.control-group:last').clone());
-                $('.control-group:last input').val('');
 
+            reindex=function(){
                 $('.control-group').each(function(idx, el){
                     $(el).find('input').each(function(idx2, el2){
                         $(el2).attr('id',$(el2).attr('class') + "-" + idx);
                         $(el2).attr('name', $(el2).attr('class') + "_" + idx);
                     });
                 });
+            };
+
+            $('.btn-remove').click(function(el){
+                $(el).closest('.control-group').remove();
+                reindex();
+            });
+
+            $('#btn-add').click(function() {
+                $('.control-group:last').after($('.control-group:last').clone());
+                $('.control-group:last input').val('');
+                reindex();
             });
         });
       }
